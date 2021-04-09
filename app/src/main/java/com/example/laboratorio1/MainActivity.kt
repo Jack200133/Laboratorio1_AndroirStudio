eb01d908a6f6cc6ee02f7b11279ecae36a1e8d32
Jack200133 <car20593@uvg.edu.gt>
2021-04-09 15:15:19 -0600

    Comentario 3.0 para el MainActivity

--- app/src/main/java/com/example/laboratorio1/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio1/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Este es el gran cambio para el tercer commit
+        //Este es el gran cambio para el cuarto commit para feature_1
     }
 }
